b78c56fc50d125677db4e1b335b3e082c39a4b3a
Joseph Miringu <jmiringu384@gmail.com>
2026-03-11 02:05:32 +0300

    Set per-user desktop wallpapers

--- src/components/desktop/Desktop.tsx
+++ src/components/desktop/Desktop.tsx
@@ -4,11 +4,12 @@ import Taskbar from "./Taskbar";
 import WindowManager from "@/components/window/WindowManager";
 
 export default function Desktop() {
-  const { setStartMenuOpen } = useDesktop();
+  const { user, setStartMenuOpen } = useDesktop();
 
   return (
     <div
       className="fixed inset-0 desktop-wallpaper overflow-hidden"
+      style={user ? { backgroundImage: `url('${user.wallpaper}')` } : undefined}
       onClick={() => setStartMenuOpen(false)}
     >
       <DesktopIcons />
